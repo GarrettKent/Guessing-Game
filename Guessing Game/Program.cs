86fee19c74e3f64996ea825c8d5bb08faafca407
GarrettKent <46579862+GarrettKent@users.noreply.github.com>
2019-08-06 18:32:02 -0500

    Add files via upload

---
+++ Guessing Game/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Guessing_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretWord = "giraffe";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 3;
+            bool outOfGuesses = false;
+
+            while (guess != secretWord && !outOfGuesses)
+            {
+                if (guessCount < guessLimit)
+                {
+                    Console.Write("Enter Guess:");
+
+                    guess = Console.ReadLine();
+
+                    guessCount++;
+                }
+                else
+                {
+                    outOfGuesses = true;
+                }
+
+                if (outOfGuesses)
+                {
+                    Console.WriteLine("You Lose");
+                    Console.ReadLine();
+                }
+            }
+            Console.Write("You Win!");
+        }
+    }
+}
